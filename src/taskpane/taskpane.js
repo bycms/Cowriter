55186c9fbad3a383a9647162e960add8b6a30808
BoyuanChen-1113 <cby2022.1113@gmail.com>
2024-08-19 20:20:19 +0800

    import

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,5 +1,5 @@
-import { GoogleGenerativeAI } from '@google/generative-ai';
-import markdownIt from 'markdown-it';
+import { GoogleGenerativeAI } from '/node_modules/@google/generative-ai';
+import markdownIt from '/node_modules/markdown-it';
 
 let welcome = document.getElementsByClassName("system");
 let textbox = document.getElementById("chat-input-text");
